c826c968a5de15d7ea1afdb9450aab870f672218
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-12-31 13:42:32 +0300

    Add localStorage for goods

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,114 +5,30 @@ import Basket from "./Basket";
 import Cards from "./Cards";
 import { useLabel } from "./LabelContext";
 import { ICard } from "../types";
-import { useState } from "react";
-
-const data = [
-  {
-    id: 1,
-    image: "card-1",
-    price: 689,
-    title: "Мясная бомба",
-    weight: 520,
-    category: "Бургеры",
-  },
-  {
-    id: 2,
-    image: "card-2",
-    price: 550,
-    title: "Супер сырный",
-    weight: 512,
-    category: "Бургеры",
-  },
-  {
-    id: 3,
-    image: "card-3",
-    price: 639,
-    title: "Сытный",
-    weight: 580,
-    category: "Бургеры",
-  },
-  {
-    id: 4,
-    image: "card-4",
-    price: 480,
-    title: "Тяжелый удар",
-    weight: 470,
-    category: "Бургеры",
-  },
-  {
-    id: 5,
-    image: "card-5",
-    price: 450,
-    title: "Вечная классика",
-    weight: 450,
-    category: "Бургеры",
-  },
-  {
-    id: 6,
-    image: "card-6",
-    price: 560,
-    title: "Итальянский",
-    weight: 510,
-    category: "Бургеры",
-  },
-  {
-    id: 7,
-    image: "card-7",
-    price: 250,
-    title: "Начос",
-    weight: 220,
-    category: "Закуски",
-  },
-  {
-    id: 8,
-    image: "card-8",
-    price: 245,
-    title: "Картошка фри",
-    weight: 180,
-    category: "Закуски",
-  },
-  {
-    id: 9,
-    image: "card-9",
-    price: 230,
-    title: "Луковые кольца",
-    weight: 160,
-    category: "Закуски",
-  },
-  {
-    id: 10,
-    image: "card-10",
-    price: 290,
-    title: "Домашний хот-дог",
-    weight: 250,
-    category: "Хот-доги",
-  },
-  {
-    id: 11,
-    image: "card-11",
-    price: 239,
-    title: "Жгучий хот-дог",
-    weight: 245,
-    category: "Хот-доги",
-  },
-  {
-    id: 12,
-    image: "card-12",
-    price: 220,
-    title: "Классический хот-дог",
-    weight: 215,
-    category: "Хот-доги",
-  },
-];
+import { useEffect, useState } from "react";
+import { goods } from "../mock/goods";
 
 const Main = () => {
   const { selectedLabel } = useLabel();
   const [basketData, setBasketData] = useState<ICard[]>([]);
   const [addedItems, setAddedItems] = useState<number[]>([]);
 
+  useEffect(() => {
+    const savedBasket = localStorage.getItem("basket");
+
+    if (savedBasket) {
+      const parsedBasket: ICard[] = JSON.parse(savedBasket);
+
+      setBasketData(parsedBasket);
+      setAddedItems(parsedBasket.map((card) => card.id));
+    }
+  }, []);
+
   const addToBasket = (card: ICard) => {
-    setBasketData((prevBasketData) => [...prevBasketData, card]);
+    const updatedBasket = [...basketData, card];
+    localStorage.setItem("basket", JSON.stringify(updatedBasket));
+
+    setBasketData(updatedBasket);
     setAddedItems((prevAddedItems) => [...prevAddedItems, card.id]);
   };
 
@@ -121,7 +37,7 @@ const Main = () => {
       <MainInner>
         <Basket cards={basketData} />
         <Cards
-          cards={data}
+          cards={goods}
           selectedLabel={selectedLabel}
           addToBasket={addToBasket}
           addedItems={addedItems}
